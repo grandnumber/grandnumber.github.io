41b40d889b43f48a74633f6c3aa0ad028c3d175e
grandnumber <thegrandnumber@gmail.com>
2015-01-17 00:27:34 -0800

    add t7

---
+++ javascriptscratch.js
@@ -0,0 +1,7 @@
+var my_array = [1,2,3];
+
+var result = 0;
+
+for(var i=0; i<my_array.length; i++) {result += my_array[i];}
+
+console.log(result);
---
+++ rubyscratch.rb
@@ -0,0 +1,7 @@
+
+my_array = [1,2,3]
+
+
+sum = 0 
+my_array.each { |x| sum +=x  }
+p sum 
